6f7c4998bf0756c6c696e6ca45013e252ae6c4b5
Mr Plankton <ajisubarkah99@gmail.com>
2018-12-12 18:43:13 +0700

    Initial commit

---
+++ app/src/main/java/android/sisgo/LoginActivity.java
@@ -0,0 +1,26 @@
+package android.sisgo;
+
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button button = findViewById(R.id.button_login);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, DashboardActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
